ebceaa602d346eaa7aa35b33b728c48fd5365452
rfle <romanfleischanderl@Romans-MacBook-Pro.local>
2015-03-04 15:04:47 +0100

    further

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ console.log("Ein JS Beispiel");
 console.log("Ist abeer nicht fertig");
 alert("DO NOT USE IN PRODUCTION");
 alert("BAD THINGS WILL HAPPEN");
+
+document.body.style.backgroundColor = "red";
+
+var yes = confirm("Are you sure you don´t wan´t this in production");
+
+if(!yes) {
+	window.close();
+}
